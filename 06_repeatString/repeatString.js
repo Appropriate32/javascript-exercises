8c774fe8f98be89f738dde2757bfd6abfc1bb399
Appropriate32 <frostghost32@gmail.com>
2025-07-01 00:15:16 +0500

    did it

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(word, numberTimes) {
+     let wordAdd = "";
+     for (let i = 0; i < numberTimes; i++) {
+        wordAdd += word
+     }
+     return wordAdd;
 };
 
+repeatString("hey", 5)
 // Do not edit below this line
 module.exports = repeatString;
