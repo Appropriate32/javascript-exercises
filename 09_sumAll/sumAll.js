b3f5b43ca59d132355b11ea43a400c7c5aa56170
Appropriate32 <frostghost32@gmail.com>
2025-07-01 23:20:25 +0500

    did it :D

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, secondNum) {
+    let total = 0;
+    if (!Number.isInteger(firstNum) || !Number.isInteger(secondNum)) {
+       return 'ERROR'
+    }
+    if (firstNum < 0 || secondNum < 0) {
+        return 'ERROR'
+    }
+    if (firstNum < secondNum) {
+        for (let i = firstNum; i <= secondNum; i ++) {
+            total += i;
+        }
+    } else if (firstNum > secondNum) {
+        for (let i = secondNum; i <= firstNum; i++) {
+            total += i;
+        }
+    }
+   
+    return total;
 };
 
 // Do not edit below this line
