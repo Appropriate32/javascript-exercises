f54019669f3b1fa41c279ddcc2aafaa3c4579da8
Appropriate32 <frostghost32@gmail.com>
2025-07-18 01:41:08 +0500

    Works :D

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	   return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	 return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+    let total = 0;
+	  arr.forEach(element => {
+        total += element;
+    });
+    return total;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+      return numbers.reduce((total, num) => total * num, 1);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	   return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+     let fact = 1;
+	   for (let i = 1; i <= num; i++) {
+         fact = fact * i;
+     };
+     return fact;
 };
 
 // Do not edit below this line
