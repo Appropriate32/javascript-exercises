ff18436154432da30c4fcfaba80a595cc1c25d48
Appropriate32 <frostghost32@gmail.com>
2025-07-19 21:37:17 +0500

    done :D

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(objsArray) {
+    let titleArray = [];
+     objsArray.forEach((element, index) => {
+         titleArray[index] = element.title;
+     });
+     return titleArray;
 };
 
 // Do not edit below this line
