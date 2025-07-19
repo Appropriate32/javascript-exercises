789b9b03a855e13c1787dc9e055fb02bc2c1d56f
Appropriate32 <frostghost32@gmail.com>
2025-07-19 22:12:16 +0500

    done :D

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArray) {
+      peopleArray.sort((a , b) => {
+      const currentDate = new Date();
+      const currentYear = currentDate.getFullYear();
 
+      let ageOfB = (b.yearOfDeath ?? currentYear) - b.yearOfBirth;
+      let ageOfA = (a.yearOfDeath ?? currentYear) - a.yearOfBirth;
+      
+      return ageOfB - ageOfA;
+      })
+      return peopleArray[0];
 };
 
 // Do not edit below this line
